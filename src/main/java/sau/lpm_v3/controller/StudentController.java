2f0d8eafafa49253db2fc7c0f56aac8f1a3749ea
Ahmet Oğan <ahmetoganasd@gmail.com>
2026-03-28 20:50:18 +0300

    feat: add student/all.html and convert its controller

--- src/main/java/sau/lpm_v3/controller/StudentController.java
+++ src/main/java/sau/lpm_v3/controller/StudentController.java
@@ -1,6 +1,8 @@
 package sau.lpm_v3.controller;
 
 import sau.lpm_v3.dtos.StudentDTO;
+import sau.lpm_v3.exception.ErrorMessages;
+import sau.lpm_v3.exception.ResourceNotFoundException;
 import sau.lpm_v3.model.Place;
 import sau.lpm_v3.model.Student;
 import sau.lpm_v3.service.StudentServiceImpl;
@@ -8,13 +10,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/student")
 public class StudentController {
+
+    //For Logging Requirement
     private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
     private StudentServiceImpl studentService;
 
@@ -22,38 +28,82 @@ public class StudentController {
         this.studentService = studentService;
     }
 
+    /*
     @GetMapping("/all")
     public ResponseEntity<List<StudentDTO>> getAllStudents(){
         logger.info("Get all student");
         return new ResponseEntity<>(studentService.getAllStudents(), HttpStatus.OK);
     }
+    */
 
+    @GetMapping("all")
+    public String getAllStudents(Model model){
+        List<StudentDTO> students = studentService.getAllStudents();
+        model.addAttribute("students", students);
+        return "students/all";
+    }
+
+    /*
     @GetMapping(value = "/get/{id}", produces = "application/json")
     public ResponseEntity<StudentDTO> getStudent(@PathVariable Long id) {
         if (id == null || id == 0) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         logger.info("Get student by id {}", id);
         return new ResponseEntity<>(studentService.getStudentById(id), HttpStatus.OK);
     }
+    */
 
-    @PostMapping(value = "/add", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<StudentDTO> addStudent(@RequestBody Student student) {
-        logger.info("Creating student {}", student.getId());
-        return new ResponseEntity<>(studentService.createStudent(student), HttpStatus.CREATED);
+    @GetMapping(value = "/{id}", produces = "application/json")
+    public String getStudent(@PathVariable Long id, Model model){
+        if(id==null || id == 0) return ErrorMessages.ERROR_STUDENT_NOT_FOUND;
+        model.addAttribute("student", studentService.getStudentById(id));
+        return "students/_show";
     }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity<StudentDTO> updateStudent(@PathVariable Long id, @RequestBody Student student) {
-        if (id == null || id == 0 || student == null) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        logger.info("Updating student {}", id);
-        return new ResponseEntity<>(studentService.updateStudent(id, student), HttpStatus.OK);
+    @GetMapping(value = "/add")
+    public String addStudent(Model model) {
+        model.addAttribute("student", new StudentDTO());
+        return "students/_add";
+    }
+
+    @PostMapping(value = "/add")
+    public String userAdd(@ModelAttribute("student") StudentDTO studentDto) {
+        //Logs when a new entity
+        logger.info("Added a new student: {}", studentDto.getName());
+
+        //Before sending DTO to Service, converts to Entity
+        Student student = new Student();
+        student.setName(studentDto.getName());
+        student.setDepartment(studentDto.getDepartment());
+
+        studentService.createStudent(student);
+        return "redirect:/students/all";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateStudent(@PathVariable Long id, Model model) {
+        //  Already getStudentById converts DTO
+        model.addAttribute("student", studentService.getStudentById(id));
+        return "students/_update";
+    }
+
+    @PostMapping("/update")
+    public String updateStudent(@ModelAttribute("student") StudentDTO studentDto) {
+        //Before sending DTO to Service, converts to Entity
+        Student student = new Student();
+        student.setName(studentDto.getName());
+        student.setDepartment(studentDto.getDepartment());
+
+        studentService.updateStudent(student.getId(),student);
+        return "redirect:students/all";
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Place> deleteStudent(@PathVariable Long id) {
+    public ResponseEntity<String> deleteStudent(@PathVariable Long id) {
         if (id == null || id == 0) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         studentService.deleteStudent(id);
-        logger.info("Deleting student {}", id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
+
+
 
