be9ec7131491a0b9c737f7eba0a752185baf88b8
Ahmet Oğan <ahmetoganasd@gmail.com>
2026-03-29 16:07:15 +0300

    feat(place): add UI for place CRUD operations

--- src/main/java/sau/lpm_v3/controller/StudentController.java
+++ src/main/java/sau/lpm_v3/controller/StudentController.java
@@ -1,11 +1,7 @@
 package sau.lpm_v3.controller;
 
 import sau.lpm_v3.dtos.StudentDTO;
-import sau.lpm_v3.exception.ErrorMessages;
-import sau.lpm_v3.exception.ResourceNotFoundException;
-import sau.lpm_v3.model.Place;
-import sau.lpm_v3.model.Student;
-import sau.lpm_v3.service.StudentServiceImpl;
+import sau.lpm_v3.service.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -22,39 +18,21 @@ public class StudentController {
 
     //For Logging Requirement
     private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
-    private StudentServiceImpl studentService;
+    private StudentService studentService;
 
-    public StudentController(StudentServiceImpl studentService) {
+    public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
-    /*
-    @GetMapping("/all")
-    public ResponseEntity<List<StudentDTO>> getAllStudents(){
-        logger.info("Get all student");
-        return new ResponseEntity<>(studentService.getAllStudents(), HttpStatus.OK);
-    }
-    */
-
     @GetMapping("all")
     public String getAllStudents(Model model){
-        List<StudentDTO> students = studentService.getAllStudents();
-        model.addAttribute("students", students);
+        List<StudentDTO> studentDtos = studentService.getAllStudents();
+        model.addAttribute("students", studentDtos);
         return "students/all";
     }
 
-    /*
-    @GetMapping(value = "/get/{id}", produces = "application/json")
-    public ResponseEntity<StudentDTO> getStudent(@PathVariable Long id) {
-        if (id == null || id == 0) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        logger.info("Get student by id {}", id);
-        return new ResponseEntity<>(studentService.getStudentById(id), HttpStatus.OK);
-    }
-    */
-
     @GetMapping(value = "/{id}", produces = "application/json")
     public String getStudent(@PathVariable Long id, Model model){
-        if(id==null || id == 0) return ErrorMessages.ERROR_STUDENT_NOT_FOUND;
         model.addAttribute("student", studentService.getStudentById(id));
         return "students/_show";
     }
@@ -97,8 +75,6 @@ public class StudentController {
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteStudent(@PathVariable Long id) {
-        if (id == null || id == 0) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-
         // Logs when deletint an entity
         // It is going to add USER DETAILS who performed to action
         logger.warn("Student [{}] DELETED", studentService.getStudentById(id).getName());
